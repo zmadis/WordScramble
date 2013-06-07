8583497ebc057c161bfa423c850192829d43bfe2
zmadis <zmadis@sbcglobal.net>
2013-06-06 22:24:55 -0700

    Changed width of text input

--- WordScramble/gen/cgu/edu/ist380/wordscramble/R.java
+++ WordScramble/gen/cgu/edu/ist380/wordscramble/R.java
@@ -26,11 +26,14 @@ public final class R {
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f080006;
-        public static final int action_settings=0x7f080009;
+        public static final int action_settings=0x7f08000c;
         public static final int button1=0x7f080008;
         public static final int button2=0x7f080007;
+        public static final int button3=0x7f08000b;
+        public static final int editText1=0x7f08000a;
         public static final int imageView1=0x7f080003;
         public static final int information=0x7f080001;
+        public static final int linearLayout1=0x7f080009;
         public static final int resume=0x7f080000;
         public static final int start=0x7f080002;
         public static final int textView1=0x7f080005;
